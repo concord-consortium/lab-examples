8a6dba8533c0f7e4cf747d13fa8c5072ae702c2f
Aaron Unger <aaron.unger@gmail.com>
2014-07-25 13:23:50 -0700

    Add some helpers for debugging the dataset
    In the console, you can run:
    `sensorPhone.post('getDataset', 'sensor-dataset');`

--- pages/10-sensor-prediction.js
+++ pages/10-sensor-prediction.js
@@ -1,11 +1,13 @@
 // Call this function when jQuery thinks the page is ready.
+var predictionPhone, sensorPhone;
+
 $(function () {
   // Intitialization.
-  var predictionPhone = new iframePhone.ParentEndpoint($('#prediction-iframe')[0], function () {
+  predictionPhone = new iframePhone.ParentEndpoint($('#prediction-iframe')[0], function () {
     console.log("Connection with prediction Lab iframe established.");
   });
 
-  var sensorPhone = new iframePhone.ParentEndpoint($('#sensor-iframe')[0], function () {
+  sensorPhone = new iframePhone.ParentEndpoint($('#sensor-iframe')[0], function () {
     console.log("Connection with sensor Lab iframe established.");
   });
 
@@ -17,6 +19,14 @@ $(function () {
         sensorPhone.post('sendDatasetEvent', {eventName: eventName, datasetName: 'sensor-dataset', data: evt.data });
       });
       predictionPhone.post('listenForDatasetEvent', {eventName: eventName, datasetName: 'prediction-dataset'});
+
+      // For debugging...
+      predictionPhone.addListener('dataset', function(evt) {
+        console.log("dataset", evt);
+      });
+      sensorPhone.addListener('dataset', function(evt) {
+        console.log("dataset", evt);
+      });
   };
 
   var setupCoordination = function() {
