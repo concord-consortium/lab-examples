30f0b43b2d778936d59ac0cb937057b9be70e908
Aaron Unger <aaron.unger@gmail.com>
2014-07-28 11:36:49 -0700

    Remove unused code

--- pages/11-click-to-play-prediction.js
+++ pages/11-click-to-play-prediction.js
@@ -31,9 +31,6 @@
     var predictionPhone = phones[pred_dom],
         sensorPhone     = phones[sensor_dom];
 
-    var predictionModelLoaded = false,
-        sensorModelLoaded = false;
-
     var _registerRelay = function(eventName) {
         predictionPhone.addListener("prediction-dataset-"+eventName, function(evt) {
           sensorPhone.post('sendDatasetEvent', {eventName: eventName, datasetName: 'sensor-dataset', data: evt.data });
@@ -47,21 +44,6 @@
       for (i = 0; i < events.length; i++) {
         _registerRelay(events[i]);
       }
-    };
-
-    var initializeInteractives = function() {
-      predictionPhone.addListener('modelLoaded', function(){
-        predictionModelLoaded = true;
-        if (sensorModelLoaded) {
-          setupCoordination();
-        }
-      });
-      sensorPhone.addListener('modelLoaded', function(){
-        sensorModelLoaded = true;
-        if (predictionModelLoaded) {
-          setupCoordination();
-        }
-      });
     };
 
     setupCoordination();
