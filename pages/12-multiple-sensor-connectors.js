196462e178269316470750d844c66ad673e2be5c
Aaron Unger <aaron.unger@gmail.com>
2014-11-03 12:12:03 -0800

    Add a 3rd sensor for multiple sensor connector example.
    Also, fix the title and description.

--- pages/12-multiple-sensor-connectors.js
+++ pages/12-multiple-sensor-connectors.js
@@ -1,5 +1,5 @@
 // Call this function when jQuery thinks the page is ready.
-var sensor1Phone, sensor2Phone;
+var sensor1Phone, sensor2Phone, sensor3Phone;
 
 $(function () {
   // Intitialization.
@@ -11,12 +11,19 @@ $(function () {
     console.log("Connection with sensor2 Lab iframe established.");
   });
 
+  sensor3Phone = new iframePhone.ParentEndpoint($('#sensor3-iframe')[0], function () {
+    console.log("Connection with sensor3 Lab iframe established.");
+  });
+
   var initializeInteractives = function() {
     sensor1Phone.addListener('modelLoaded', function(){
       sensor1Phone.post('set', {name: 'clientName', value: "Sensor1"});
     });
     sensor2Phone.addListener('modelLoaded', function(){
       sensor2Phone.post('set', {name: 'clientName', value: "Sensor2"});
+    });
+    sensor3Phone.addListener('modelLoaded', function(){
+      sensor3Phone.post('set', {name: 'clientName', value: "Sensor3"});
     });
   };
 
